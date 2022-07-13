9e0cd01bda2fabbba2f43858113056a380396462
kyle <kyle.dick0811@gmail.com>
2022-07-12 21:57:19 -0600

    Complete rendering App component to the DOM

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,40 @@
+// import Search from '../../src/components/Search.js';
+// import VideoList from '../../src/components/VideoList.js';
+// import VideoPlayer from '../../src/components/VideoPlayer.js';
+// import exampleVideoData from '../../src/data/exampleVideoData.js';
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // console.log(props);
+//   }
+
+//   render() {
+//     console.log('exa', exampleVideoData);
+//     return (
+//       <div>
+//         <nav className="navbar">
+//           <div className="col-md-6 offset-md-3">
+//             <div><h5><em>search</em> view goes here</h5></div>
+//           </div>
+//         </nav>
+//         <div className="row">
+//           <div className="col-md-7">
+//             <div><h5><em>videoPlayer</em> view goes here</h5></div>
+//           </div>
+//           <div className="col-md-5">
+//             <div><h5><em>videoList</em> test </h5></div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +47,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em> will this work</h5></div>
       </div>
     </div>
   </div>
